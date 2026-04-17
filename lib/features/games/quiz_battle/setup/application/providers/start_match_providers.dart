23c5283dc369648849f9167ff2ce46b56f425be9
alialsayed20 <65576604+alialsayed20@users.noreply.github.com>
2026-04-18 01:00:03 +0300

    Delete lib/features/games/quiz_battle directory

--- lib/features/games/quiz_battle/setup/application/providers/start_match_providers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../services/start_match_service.dart';
-
-final Provider<StartMatchService> startMatchServiceProvider =
-    Provider<StartMatchService>((Ref ref) {
-  return const StartMatchService();
-});
